9281290f4a8c0c39985edc9a68fa5777bb8fbba1
ezioguga <ezioguga@gmail.com>
2021-02-16 11:56:13 +0530

    added login and register component

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import { User } from './../_models/user';
+import { Observable } from 'rxjs';
+import { AccountService } from './../_services/account.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
